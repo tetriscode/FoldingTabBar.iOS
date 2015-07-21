c3ba875d05d92da69b4ea5585a254f8b333f7b0c
Wes Richardet <wes@tetriscodes.com>
2015-07-21 10:45:26 -0500

    Adding Framework Build

---
+++ Framework/FoldingTabBar/FoldingTabBar.h
@@ -0,0 +1,20 @@
+//
+//  FoldingTabBar.h
+//  FoldingTabBar
+//
+//  Created by Wes Richardet on 7/21/15.
+//  Copyright (c) 2015 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FoldingTabBar.
+FOUNDATION_EXPORT double FoldingTabBarVersionNumber;
+
+//! Project version string for FoldingTabBar.
+FOUNDATION_EXPORT const unsigned char FoldingTabBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoldingTabBar/PublicHeader.h>
+
+#import "YALFoldingTabBar.h"
+#import "YALFoldingTabBarItem.h"
