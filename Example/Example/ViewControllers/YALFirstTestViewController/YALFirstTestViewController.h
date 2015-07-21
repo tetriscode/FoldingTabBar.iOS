8c69fb970afa742b601c214f66348372ed3b06b9
Wes Richardet <wes@tetriscodes.com>
2015-07-21 11:01:46 -0500

    Removing Examples Folder

--- Example/Example/ViewControllers/YALFirstTestViewController/YALFirstTestViewController.h
+++
@@ -1,9 +0,0 @@
-// For License please refer to LICENSE file in the root of YALAnimatingTabBarController project
-
-#import <UIKit/UIKit.h>
-
-#import "YALTabBarInteracting.h"
-
-@interface YALFirstTestViewController : UIViewController <YALTabBarInteracting>
-
-@end
