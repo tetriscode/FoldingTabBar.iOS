e8e274357ced78bf3d0b8f41524dbba53350405c
Wes Richardet <wes@tetriscodes.com>
2015-07-21 13:38:54 -0500

    Fixing export header

--- Framework/FoldingTabBar/FoldingTabBar.h
+++ Framework/FoldingTabBar/FoldingTabBar.h
@@ -1,11 +1,6 @@
-//
-//  FoldingTabBar.h
-//  FoldingTabBar
-//
-//  Created by Wes Richardet on 7/21/15.
-//  Copyright (c) 2015 Yalantis. All rights reserved.
-//
+// For License please refer to LICENSE file in the root of YALAnimatingTabBarController project
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 //! Project version number for FoldingTabBar.
@@ -16,7 +11,9 @@ FOUNDATION_EXPORT const unsigned char FoldingTabBarVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FoldingTabBar/PublicHeader.h>
 
-#import "YALFoldingTabBar.h"
-#import "YALFoldingTabBarItem.h"
-#import "YALAnimatingTabBarConstants.h"
-#import "YALFoldingTabBarController.h"
+#import <FoldingTabBar/YALAnimatingTabBarConstants.h>
+#import <FoldingTabBar/YALFoldingTabBar.h>
+#import <FoldingTabBar/YALFoldingTabBarController.h>
+#import <FoldingTabBar/YALSprintAnimation.h>
+#import <FoldingTabBar/YALTabBarInteracting.h>
+#import <FoldingTabBar/YALTabBarItem.h>
